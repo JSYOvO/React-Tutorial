a3d8a1b60597457f9de4484108cced70cd3671ab
unknown <wotjq5113@gmail.com>
2020-07-20 00:13:53 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress'
 import {withStyles} from '@material-ui/core/styles';
 
 const styles = theme => (
@@ -18,13 +19,21 @@ const styles = theme => (
     },
     table : {
       minWidth : 1080
+    },
+    progress : {
+      marginTop : theme.spacing(3)
     }
   }
 )
 
 class App extends Component {
+  state = {
+    customers : "",
+    completed : 0
+  }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers : res}))
       .catch(err => console.log(err))
@@ -34,11 +43,18 @@ class App extends Component {
     const body = await response.json();
     return body;
   }
-  state = {
-    customers : ""
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed : completed >= 100 ? 0 : completed + 1});
   }
 
-  
+/* React-JS Life-Cycle
+    1. constructor()
+    2. componentWillMount()
+    3. render()
+    4. componentDidMount()
+    if) props or state changed => shouldComponentUpdate()
+*/
 
   render(){
     const {classes} = this.props;
@@ -68,7 +84,18 @@ class App extends Component {
                   job = {data.job}
                 />
               )
-            }) : ""}
+            }) : 
+            <TableRow>
+              <TableCell colspan = "6" align =  "center">
+                <CircularProgress 
+                  className = {classes.progress} 
+                  varient = "determinant"
+                  value = {this.state.completed}
+                  color = "secondary"
+                />
+              </TableCell>
+            </TableRow>}
+            
           </TableBody>
         </Table>
       </Paper>

7df8308c5431933f33cc2858b2d8b5be4af2d15b
unknown <wotjq5113@gmail.com>
2020-07-20 01:19:03 +0900

    Create Customer DB Table & NodeJS DB Connection

--- server.js
+++ server.js
@@ -1,38 +1,34 @@
+const fs = require('fs');
+const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
+const { POINT_CONVERSION_HYBRID } = require('constants');
+
 const app = express();
 const port = process.env.PORT || 5000;
+const data = fs.readFileSync('./database.json')
+const conf = JSON.parse(data);
+const connection = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
+});
+connection.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/any',
-            'name' : '유재섭',
-            'birthday' : '931206',
-            'gender' : 'male',
-            'job' : 'trash'
-          },
-          {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/any2',
-            'name' : '유재섭2',
-            'birthday' : '931206',
-            'gender' : 'male',
-            'job' : 'trash'
-          },
-          {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/any3',
-            'name' : '유재섭3',
-            'birthday' : '931206',
-            'gender' : 'male',
-            'job' : 'trash'
-          }
-    ])
+    connection.query(
+        "select * from CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
 });
 
 app.listen(port, () => {
